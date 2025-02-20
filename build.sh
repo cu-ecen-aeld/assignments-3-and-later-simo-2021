b90b3c0fc89000ee27f5cbb667327956ea6b5afd
simo-2021 <at.simo@pm.me>
2025-02-20 15:49:15 +0100

    add build.sh with cmd for time reduction during exe of ./build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Script to build buildroot configuration
 #Author: Siddhant Jajoo
+#Modified by: Arnaud Simo
+#Date: 17FEB2025
 
 source shared.sh
 
@@ -19,15 +21,14 @@ then
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+		make  -j$(nproc) -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} BR2_DL_DIR=/home/tchuinkou/downloads-cache
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
+		make -j$(nproc) -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}  BR2_DL_DIR=/home/tchuinkou/downloads-cache
 	fi
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
-
+	make -j$(nproc) -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}  BR2_DL_DIR=/home/tchuinkou/downloads-cache
 fi
