858982cad4d4aa51b2e9c147318d2e0b5a552991
simo-2021 <at.simo@pm.me>
2025-03-03 15:50:19 +0100

    buildroot assignments-bas upload.everything was good: make, ./build.sh  ,/runqemu.sh

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
 include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
-=======
-# external.mk
-BASE_EXTERNAL_TOP_DIR := $(call select_from_repo,$(BR2_EXTERNAL_PROJECT_BASE_PATH),$(CURDIR))
-
-BASE_EXTERNAL_PKGS_DIR := $(BASE_EXTERNAL_TOP_DIR)/package
-BASE_EXTERNAL_CONFIG_DIR := $(BASE_EXTERNAL_TOP_DIR)/configs
-
-# Inclusion des règles de construction pour les paquets
-include $(sort $(wildcard $(BASE_EXTERNAL_PKGS_DIR)/*/*.mk))
->>>>>>> ac5bd35 (add buildroot and some files(build.sh))
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,12 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = adb4b0c9146ef04fc2d85004d7fc28a620bd10b1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-simo-2021.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -19,10 +20,27 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+#define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+#    # Créer les répertoires de configuration
+#    $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf/
+#    # Copier les fichiers de configuration
+#    $(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+#    # Installer les scripts et exécutables
+#    $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+#    $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+#    $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+#    # Copier les fichiers de test si nécessaire
+#    $(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+#endef
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
